0f402bf32ac9e594e71e78a2bc2c15f7aba537aa
Aaron Rodriguez <adashrod@gmail.com>
2025-12-21 09:57:21 -0800

    Strategy 2: @container style queries with custom properties (no CSS if())

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from "@angular/router";
 
 import { Strategy1CssIfComponent } from "app/components/strategy1-css-if/strategy1-css-if.component";
+import { Strategy2ContainerStyleComponent } from "app/components/strategy2-container-style/strategy2-container-style.component";
 import { WelcomeComponent } from "app/components/welcome/welcome.component";
 
 export const routes: Routes = [{
@@ -9,4 +10,7 @@ export const routes: Routes = [{
 }, {
     path: "strategy-1",
     component: Strategy1CssIfComponent
+}, {
+    path: "strategy-2",
+    component: Strategy2ContainerStyleComponent
 }];
---
+++ src/app/components/strategy2-container-style/strategy2-container-style.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-strategy2-container-style",
+    standalone: true,
+    imports: [],
+    templateUrl: "./strategy2-container-style.component.html",
+    styleUrl: "./strategy2-container-style.component.css"
+})
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
+export class Strategy2ContainerStyleComponent {}
